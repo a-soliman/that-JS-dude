2261c356a69a3a59db14cc42b19e817f3968b6d9
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-03-15 12:23:07 -0700

    solved isPrime proplem

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 /*
 1. verify a prime number.
 */
+function isPrime(n) {
+  var divisor = 3;
+  var limit = Math.sqrt(n);
+  
+  if(n === 2 || n === 3) {
+    return true;
+  } 
+  if(n % 2 === 0) {
+    return false;
+  }
+  while(divisor <= limit) {
+    if(n % divisor === 0) {
+      return false;
+    } else {
+      divisor +=2;
+    }
+  }
+  return true;
+}
+
+console.log('1. isPrime : ' + isPrime(11));
+
+//===============================================================================
